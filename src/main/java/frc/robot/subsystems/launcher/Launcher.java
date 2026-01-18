84a265b7444acbc5ffa7c0a486351e1e4e86ab39
Gavan Bess <gavanbess@gmail.com>
2026-01-17 23:48:23 -0800

    capital E

--- src/main/java/frc/robot/subsystems/launcher/Launcher.java
+++ src/main/java/frc/robot/subsystems/launcher/Launcher.java
@@ -1,18 +1,45 @@
 package frc.robot.subsystems.launcher;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Launcher extends SubsystemBase {
     
+    public enum LauncherState {
+        SHOOTING,
+        IDLE
+    }
+
+    LauncherState state = LauncherState.SHOOTING;
+    LauncherState oldState = LauncherState.IDLE;
+    
     LauncherIO io;
     LauncherIOInputsAutoLogged in = new LauncherIOInputsAutoLogged();
 
-    public Launcher(){
+    public Launcher() {
 
     }
 
     @Override
-    public void periodic(){
+    public void periodic() {
+        io.updateInputs(in);
+        Logger.processInputs("Launcher", in);
+        Logger.recordOutput("Launcher/state", state);
+        Logger.recordOutput("Launcher/oldState", oldState);
 
+        switch (state) {
+            case SHOOTING:
+                break;
+            case IDLE:
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void setState(LauncherState wanted){
+        oldState = state;
+        state = wanted;
     }
 }
