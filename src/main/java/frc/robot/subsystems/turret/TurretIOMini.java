a3e5032577110a53bd69644e2a09d91bb097037b
potato <potatainator@proton.me>
2026-02-05 09:59:29 -0800

    started turretIOMini

---
+++ src/main/java/frc/robot/subsystems/turret/TurretIOMini.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems.turret;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Voltage;
+
+public class TurretIOMini implements TurretIO {
+    
+    @Override
+    public void updateInputs(TurretIOInputs input){
+
+    }    
+    
+    @Override
+    public void setGoal(Angle goal){
+
+    }
+    
+    @Override
+    public void setVout(Voltage vout){
+
+    }
+    
+    @Override
+    public void setOpenLoop(boolean openloop){
+
+    }
+    
+}
