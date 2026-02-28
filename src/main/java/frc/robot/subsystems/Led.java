9a907623aca9f8b0c298fc025382f606bebfca2e
potato <potatainator@proton.me>
2026-02-28 13:39:24 -0800

    robolead

--- src/main/java/frc/robot/subsystems/Led.java
+++ src/main/java/frc/robot/subsystems/Led.java
@@ -6,29 +6,31 @@ import org.littletonrobotics.junction.networktables.LoggedNetworkBoolean;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.AddressableLEDBufferView;
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Led extends SubsystemBase {
 
     private AddressableLED led = new AddressableLED(0);
-    private AddressableLEDBuffer buffer = new AddressableLEDBuffer(50);
-    private AddressableLEDBufferView side = new AddressableLEDBufferView(buffer, 0, 5);
-    private AddressableLEDBufferView turret = new AddressableLEDBufferView(buffer, 5, 10);
+    private AddressableLEDBuffer buffer = new AddressableLEDBuffer(71+47);
+    private AddressableLEDBufferView side = new AddressableLEDBufferView(buffer, 71, 71+46);
+    private AddressableLEDBufferView turret = new AddressableLEDBufferView(buffer, 0, 71);
 
     private LoggedNetworkBoolean rainbow = new LoggedNetworkBoolean("Debug/LED override", false);
-
-
 
     public Led() {
         led.setLength(buffer.getLength());
 
         led.start();
+        
     }
 
     @Override
     public void periodic() {
 
-        
+        LEDPattern.rainbow(255, 255).applyTo(buffer);
+        LEDPattern.solid(Color.kOrange).applyTo(side);
 
         led.setData(buffer);
 
