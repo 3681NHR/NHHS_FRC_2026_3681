20b399bea1e60155bde9c3291c70595a46ad068d
potatinator <potatinator@proton.me>
2026-03-02 08:10:27 -0800

    e

--- src/main/java/frc/robot/subsystems/hood/HoodIOSim.java
+++ src/main/java/frc/robot/subsystems/hood/HoodIOSim.java
@@ -1,30 +1,64 @@
 package frc.robot.subsystems.hood;
 
+import static edu.wpi.first.units.Units.*;
+import static frc.robot.constants.HoodConstants.*;
+
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N2;
+import edu.wpi.first.math.system.LinearSystem;
+import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.simulation.LinearSystemSim;
+import frc.utils.controlWrappers.ProfiledPID;
+import frc.utils.controlWrappers.SimpleFF;
 
 public class HoodIOSim implements HoodIO {
+
+    private boolean homed = HOOD_HOME_ON_START;
+    private boolean openloop = false;
+
+    private Angle goal = Radians.of(0);
+    private Voltage vout = Volts.of(0);
+
+    private ProfiledPID pid = new ProfiledPID(HOOD_PID_GAINS);
+    private SimpleFF ff = new SimpleFF(HOOD_FF_GAINS);
+
+    private final LinearSystem<N2, N1, N2> model = LinearSystemId.identifyPositionSystem(HOOD_FF_GAINS.kV, HOOD_FF_GAINS.kA);
+    private final LinearSystemSim<N2, N1, N2> sim = new LinearSystemSim<N2, N1, N2>(model, 0.01, 0.1);
+
+    public HoodIOSim(){
+        
+    }
     
     @Override
     public void updateInputs(HoodIOInputs input){
-        
-        input.angle = 
-        input.velocity = 
+        sim.update(0.02);
 
-        input.vout = 
-        input.current = 
-        input.temp = 
+        if(openloop){
+            vout = Volts.of(pid.calculate(sim.getOutput().get(0,0), goal.in(Rotations)));
+            vout = vout.plus(Volts.of(ff.calculate(pid.getSetpoint().velocity)));
+        }
         
-        input.homed = 
-        input.openloop = 
+        input.angle = Rotations.of(sim.getOutput().get(0,0));
+        input.velocity = RPM.of(sim.getOutput().get(0,1));
+
+        input.vout = vout;
+        input.current = Amps.of(-1);
+        input.temp = Kelvin.of(-1);
+
+        input.homed = homed;
+        input.openloop = openloop;
     }
     
     @Override
     public void setGoal(Angle goal){
-
+        openloop = false;
+        this.goal = goal;
     }
     @Override
     public void setVout(Voltage vout){
-
+        openloop = true;
+        this.vout = vout;
     }
 }
