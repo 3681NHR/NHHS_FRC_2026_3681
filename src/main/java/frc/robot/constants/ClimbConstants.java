5319e6a51d50fd9a534cd3a0be90705513a14e5e
httphypixelnet <91695346+httphypixelnet@users.noreply.github.com>
2026-03-02 09:11:56 -0800

    conversion factors are ready for tuning

--- src/main/java/frc/robot/constants/ClimbConstants.java
+++ src/main/java/frc/robot/constants/ClimbConstants.java
@@ -9,6 +9,8 @@ public class ClimbConstants {
     public static final PIDGains.ProfiledPID CLIMB_PID_GAINS = new PIDGains.ProfiledPID(1, 1, 1, 100, 100).makeTunable("Climber PID");
     public static final PIDGains.GravityFF FF = new PIDGains.GravityFF(0, 0, 0, 0).makeTunable("Climber FF");
     public static final PIDGains.GravityFF CLIMB_ID_GAINS = new PIDGains.GravityFF(0.1, 0.1, 0.1, 0.01);
-    public static final double POSITION_CONVERSION_FACTOR = 1.0;
+    private static final double GEAR_RATIO = 80.0;
+    private static final double SPOOL_CIRC = Math.PI * 1.0; // get actual circumference later
+    public static final double POSITION_CONVERSION_FACTOR = SPOOL_CIRC/GEAR_RATIO;
     public static final double VELOCITY_CONVERSION_FACTOR = 1.0;
 }
