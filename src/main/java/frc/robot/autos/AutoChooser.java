a7151e5cc6b60fb463a5ac51455a9bffe0ced761
potato <potatainator@proton.me>
2026-03-21 08:59:20 -0700

    e

--- src/main/java/frc/robot/autos/AutoChooser.java
+++ src/main/java/frc/robot/autos/AutoChooser.java
@@ -119,6 +119,7 @@ public class AutoChooser {
 //            new AutoProgram("test",  AutoFactory::createExamplePPAuto),
             new AutoProgram("preload",  AutoFactory::createPreloadAuto),
             new AutoProgram("trench depot",  AutoFactory::createDepotAuto),
+            new AutoProgram("trench depot + AI",  AutoFactory::createDepotAIAuto),
             new AutoProgram("Left insanity",  AutoFactory::createLeftPassAuto),
             new AutoProgram("Right not very insanity",  AutoFactory::createRightPassAuto),
             new AutoProgram("AI right mid",  AutoFactory::createRightAIMidAuto),
